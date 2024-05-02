6c94c62083f89ded4a12003246aedcc0ac5dc62e
Claire <claire.github-309c@sitedethib.com>
2024-05-02 22:56:21 +0200

    Fix not being able to block a subdomain of an already-blocked domain through the API (#30119)

--- spec/controllers/api/v1/admin/domain_blocks_controller_spec.rb
+++ spec/controllers/api/v1/admin/domain_blocks_controller_spec.rb
@@ -163,7 +163,44 @@ RSpec.describe Api::V1::Admin::DomainBlocksController, type: :controller do
       expect(DomainBlock.find_by(domain: 'foo.bar.com')).to_not be_nil
     end
 
-    context 'when a stricter domain block already exists' do
+    context 'when a looser domain block already exists on a higher level domain' do
+      let(:params) { { domain: 'foo.bar.com', severity: :suspend } }
+
+      before do
+        Fabricate(:domain_block, domain: 'bar.com', severity: :silence)
+      end
+
+      it 'creates a domain block with the expected domain name and severity', :aggregate_failures do
+        subject
+
+        body = body_as_json
+
+        expect(response).to have_http_status(200)
+        expect(body).to match a_hash_including(
+          {
+            domain: 'foo.bar.com',
+            severity: 'suspend',
+          }
+        )
+
+        expect(DomainBlock.find_by(domain: 'foo.bar.com')).to be_present
+      end
+    end
+
+    context 'when a domain block already exists on the same domain' do
+      before do
+        Fabricate(:domain_block, domain: 'foo.bar.com', severity: :silence)
+      end
+
+      it 'returns existing domain block in error', :aggregate_failures do
+        subject
+
+        expect(response).to have_http_status(422)
+        expect(body_as_json[:existing_domain_block][:domain]).to eq('foo.bar.com')
+      end
+    end
+
+    context 'when a stricter domain block already exists on a higher level domain' do
       let(:existing_block_domain) { 'bar.com' }
 
       it 'returns http unprocessable entity' do
